79ce94d192aeceed316b9a07740e20acdd35414d
bous0056 <karim.boussaid@etudiant.univ-reims.fr>
2024-12-18 14:05:05 +0100

    feat: couleurs dans tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:["index.html", "src/*/.{css,js,jsx,ts,tsx}"],
+  content: ["index.html", "src/**/*.{css,js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: {
+        light: '#6C41F2',
+        DEFAULT: '#6C41F2',
+        dark: '#6C41F2',
+      },
+      secondary: {
+        light: '#FBE4AB',
+        DEFAULT: '#FBE4AB',
+        dark: '#FBE4AB',
+      },
+      neutral: {
+        light: '#FFFFFF',
+        DEFAULT: '#FFFFFF',
+        dark: '#272727',
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
